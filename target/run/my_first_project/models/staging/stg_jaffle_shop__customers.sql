08ef548829b1326c80c8279da456d207e4f45dcb
KanikaGaikwad <kanikagaikwad1404@gmail.com>
2025-11-04 12:39:12 -0700

    Initial commit: added staging and mart models

---
+++ target/run/my_first_project/models/staging/stg_jaffle_shop__customers.sql
@@ -0,0 +1,16 @@
+
+  create or replace   view raw.jaffle_shop.stg_jaffle_shop__customers
+  
+  
+  
+  
+  as (
+    
+
+select
+	id as customer_id,
+	first_name,
+	last_name
+from raw.jaffle_shop.customers
+  );
+
